287191384b05f876e5e8b4920f656cdb8e71c26c
erfanscott <erfanscott@gmail.com>
2024-02-18 10:06:13 +0330

    more of librarian view

--- library-react-folder-git/library-react/src/components/LibrarianView.js
+++ library-react-folder-git/library-react/src/components/LibrarianView.js
@@ -20,90 +20,18 @@ export default function LibrarianView({ logout }) {
   const [isSearchModalVisible, setIsSearchModalVisible] = useState(false);
   const [isUpdateModalVisible, setIsUpdateModalVisible] = useState(false);
 
-  async function borrowBook(bookId) {
-    try {
-      await new MemberService({}).borrowBook(currentUser.id, bookId);
-      loadBorrowedBooks();
-    } catch (error) {
-      toast.error("Something went wrong");
-    }
-    setIsBorrowModalVisible(false);
-  }
-  async function returnBook(bookId) {
-    try {
-      await new MemberService({}).returnBook(currentUser.id, bookId);
-      loadBorrowedBooks();
-    } catch (error) {
-      toast.error("Something went wrong");
-    }
-  }
-  async function searchBook(searchKey) {
-    try {
-      const searchResult = await BookService.searchBook(searchKey);
-      return searchResult;
-    } catch (error) {
-      toast.error("Something went wrong");
-      return [];
-    }
-  }
-  async function updateProfile(formData) {
-    try {
-      await UserService.updateProfile(formData, "MEMBER", currentUser.id);
-      toast.success("Your profile has been successfully updated");
-      setIsUpdateModalVisible(false);
-      updateAuth();
-    } catch (error) {
-      toast.error(error.message);
-    }
-  }
+  const [dropDownFormData, setDropDownFormData] = useState("books");
 
-  async function loadBorrowedBooks() {
-    try {
-      const books = await new MemberService({}).getBorrowedBooks(
-        currentUser.id
-      );
-      setBorrowedBooks(books);
-    } catch (error) {
-      setBorrowedBooks([]);
-    }
-  }
-
-  useEffect(() => {
-    loadBorrowedBooks();
-  }, []);
-
-  const borrowedBooksList = borrowedBooks.map((book) => {
-    return (
-      <tr
-        key={`${book.id}`}
-        className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
-      >
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-        >
-          {book.name}
-        </th>
-        <td className="px-6 py-4">Date</td>
-
-        <td class="px-6 py-4">
-          <p
-            onClick={() => returnBook(book.id)}
-            className="cursor-pointer font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            Return
-          </p>
-        </td>
-      </tr>
-    );
-  });
+  const onDropDownFormChange = (e) => {
+    setDropDownFormData(e.target.value);
+  };
 
   return (
     <section className="bg-gray-200 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full max-h-[90vh] bg-white rounded-md sm:rounded-lg shadow dark:border md:mt-0 sm:max-w-[70%] xlg:max-w-5xl xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="relative overflow-x-auto px-6 py-6 shadow-md rounded-md sm:rounded-lg">
-            <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
+            <div className="mb-8 space-y-4">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
                 Welcome {usersName}
               </h1>
@@ -116,105 +44,64 @@ export default function LibrarianView({ logout }) {
                   Logout
                 </span>
               </h2>
+            </div>
+            <div class="flex items-start justify-between pb-4 dark:bg-gray-900">
+              {/*book/member select */}
 
-              <div>
+              <form class="max-w-sm">
+                <select
+                  id="entity"
+                  value={dropDownFormData}
+                  onChange={onDropDownFormChange}
+                  class="flex justify-center bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option value="books">Books</option>
+                  <option value="members">Members</option>
+                </select>
+              </form>
+
+              <div className="flex space-x-2">
                 <button
-                  id="dropdownActionButton"
-                  data-dropdown-toggle="dropdownAction"
-                  class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                   type="button"
+                  class="focus:outline-none text-white whitespace-nowrap bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-xs px-2 py-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                 >
-                  <span class="sr-only">Action button</span>
-                  Action
-                  <svg
-                    class="w-2.5 h-2.5 ms-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m1 1 4 4 4-4"
-                    />
-                  </svg>
+                  ADD MEMBER
                 </button>
-                {/* <!-- Dropdown menu --> */}
-                <div
-                  id="dropdownAction"
-                  class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-                >
-                  <ul
-                    class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="dropdownActionButton"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Reward
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Promote
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Activate account
-                      </a>
-                    </li>
-                  </ul>
-                  <div class="py-1">
-                    <a
-                      href="#"
-                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+
+                <div class="relative">
+                  <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg
+                      class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
                     >
-                      Delete User
-                    </a>
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
                   </div>
+                  <input
+                    onChange
+                    type="search"
+                    id="default-search"
+                    class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder={`${
+                      dropDownFormData === "books"
+                        ? "search book, author..."
+                        : "search member..."
+                    }`}
+                    required
+                  />
                 </div>
-              </div>
-              <label for="table-search" class="sr-only">
-                Search
-              </label>
-              <div class="relative">
-                <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg
-                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  id="table-search-users"
-                  class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search for users"
-                />
               </div>
             </div>
+
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
